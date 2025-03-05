3c118ebbc67d8c0b38a7a6ff588ce1a41c9ba700
Skuddin <sheikh@nextgenitltd.com>
2025-03-05 21:48:01 +0600

    update content and design

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 import About from './components/About'
 import Contact from './components/Contact'
+import Footer from './components/Footer'
 import Header from './components/Header'
 import Hero from './components/Hero'
+import Project from './components/Project'
+import Projects from './components/Project'
 import Review from './components/Review'
 import Skill from './components/Skill'
 import Work from './components/Work'
@@ -15,10 +18,12 @@ function App() {
         <Hero />
         <About />
         <Skill />
+        {/* <Project /> */}
         <Work />
         {/* <Review /> */}
         <Contact />
       </main>
+      <Footer />
     </>
   )
 }
--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,11 +6,11 @@ import Logo from '../assets/images/logo.png';
 const aboutItems = [
     {
         label: 'Project done',
-        number: 45
+        number: 10
     },
     {
         label: 'Years of experience',
-        number: 10
+        number: 2
     }
 ];
 
@@ -20,7 +20,7 @@ const About = () => {
             <div className="container">
                 <div className="bg-zinc-800/50 p-7 rounded-2xl md:p-12">
                     <p className="text-zinc-300 mb-4 md:mb-8 md:text-xl md:max-w-[60ch]">
-                        Welcome! I&apos;m Sheikh Salah Uddin, a professional web developer with a knack for crafting visually stunning and highly functional websites. Combining creativity and technical expertise. I transform your vision into digital masterpiece that excels in both appearance and performance.
+                        Welcome! I'm Sheikh Salah Uddin, a skilled Laravel and React developer passionate about building dynamic, high-performing web applications. With a strong blend of backend efficiency and frontend interactivity, I transform ideas into seamless digital experiences that are both powerful and visually engaging. Let's bring your vision to life with cutting-edge technology and top-tier performance!
                     </p>
                     <div className="flex flex-wrap items-center gap-4 md:gap-7">
                         {
